1590f97ad9531a93b0457b25ea9fe90e2d0204cb
Joseph Lansdowne <J49137@gmail.com>
2012-06-23 00:00:34 +0100

    some levels, don't have border lines

--- game/level.py
+++ game/level.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 from ext import evthandler as eh
-from ext.colhandler import HalfLine, Rect, StaticObject, Object, CollisionHandler
+from ext.colhandler import Rect, StaticObject, Object, CollisionHandler
 
 import conf
 
@@ -15,6 +15,7 @@ class Player:
                           elast = conf.PLAYER_ELAST)
         self.on_ground = False
         self.jumping = 0
+        self.jumped = False
 
     def touching_cb (self, o1, o2, dirn, i1, i2):
         assert o1 is self.obj
@@ -27,11 +28,12 @@ class Player:
 
     def jump (self, press):
         v = self.obj.vel
-        if self.jumping and not press:
-            v[1] -= conf.CONTINUE_JUMP
-        elif press and self.on_ground:
-            v[1] -= conf.INITIAL_JUMP
-            self.jumping = conf.JUMP_TIME
+        if press:
+            if self.on_ground and not self.jumping:
+                v[1] -= conf.INITIAL_JUMP
+                self.jumping = conf.JUMP_TIME
+        elif self.jumping:
+            self.jumped = True
 
     def update (self):
         vx, vy = self.obj.vel
@@ -47,6 +49,10 @@ class Player:
         dy = 1 if vy > 0 else -1
         vy -= dy * conf.AIR_RES * vy ** 2
         vy = dy * max(dy * vy, 0)
+        # jump
+        if self.jumped:
+            vy -= conf.CONTINUE_JUMP
+        self.jumped = False
         # update things
         self.obj.vel = [vx, vy]
         self.on_ground = False
@@ -61,17 +67,14 @@ class Level:
         self.ID = ID
         self.frame = conf.FRAME
         # input
-        event_handler.add_event_handlers({
-            pg.MOUSEBUTTONDOWN: self.mouse_down,
-            pg.MOUSEBUTTONUP: self.mouse_up,
-            #pg.MOUSEMOTION: self.mouse_move
-        })
         event_handler.add_key_handlers([
             (ks, [(self.move, (i,))], eh.MODE_HELD)
             for i, ks in enumerate(conf.KEYS_MOVE)
         ] + [
             (conf.KEYS_JUMP, self.jump, eh.MODE_ONDOWN_REPEAT, 1, 1),
+            ((pg.K_ESCAPE,), lambda *args: setattr(self, 'show', not self.show), eh.MODE_ONDOWN)
         ])
+        self.show = False
         self.rect = pg.Rect(0, 0, *conf.RES).inflate(2, 2)
         self.init()
 
@@ -79,18 +82,24 @@ class Level:
         data = conf.LEVELS[self.ID]
         # player
         self.player = Player(data['player_pos'])
+        # goal
+        x, y = data['goal']
+        w, h = conf.GOAL_SIZE
+        self.goal = Rect(x, y, x + w, y + h)
         # window
-        self.win = pg.Rect(data['window'])
+        x, y = self.player.rect.centre
+        w, h = conf.HALF_WINDOW_SIZE
+        self.window = pg.Rect(x - w, y - h, 2 * w, 2 * h)
         # objs
         w, h = conf.RES
-        bdy = [HalfLine(*args) for args in ((2, 0, 0, h), (3, 0, 0, w),
-                                            (0, w, 0, h), (1, h, 0, w))]
+        #bdy = [HalfLine(*args) for args in ((2, 0, 0, h), (3, 0, 0, w),
+                                            #(0, w, 0, h), (1, h, 0, w))]
         self.rects = [Rect(*args) for args in data['rects']]
-        self.objs = [StaticObject(s, conf.OBJ_ELAST) for s in self.rects + bdy]
+        self.objs = [StaticObject(s, conf.OBJ_ELAST) for s in self.rects + [self.goal]]
         # colhandler
         self.col_handler = CollisionHandler([self.player.obj] + self.objs,
-            before_cb = self.filter_col, touching_cb = self.player.touching_cb)
-        self.dragging = set()
+            self.filter_col, self.col_cb, self.player.touching_cb)
+        pg.mouse.set_pos(self.rect.center)
 
     def move (self, key, mode, mods, i):
         self.player.move(i)
@@ -98,17 +107,12 @@ class Level:
     def jump (self, key, mode, mods):
         self.player.jump(mode == 0)
 
-    def mouse_down (self, evt):
-        self.dragging.add(evt.button)
-        pg.mouse.set_pos(self.rect.center)
-
-    def mouse_up (self, evt):
-        if evt.button in self.dragging:
-            self.dragging.remove(evt.button)
-
     def filter_col (self, o1, o2, dirn, i1, i2, data):
-        c = self.win.clip
+        c = self.window.clip
         for s in (o1.shape, o2.shape):
+            if s is self.goal:
+                self.win()
+                return True
             if isinstance(s, Rect):
                 r = s.pgrect
             else: # HalfLine
@@ -119,16 +123,29 @@ class Level:
             if w == 0 and h == 0:
                 return True
 
+    def col_cb (self, o1, o2, dirn, i1, i2, I):
+        pass
+
     def update (self):
         x0, y0 = self.rect.center
         x, y = pg.mouse.get_pos()
-        if self.dragging:
-            self.win = self.win.move(x - x0, y - y0)
+        self.window = self.window.move(x - x0, y - y0)
         pg.mouse.set_pos(x0, y0)
         self.player.update()
         self.col_handler.update()
-        if not self.rect.contains(self.to_screen(self.player.rect.pgrect)):
+        # check if OoB
+        bl = self.rect.left
+        br, bb = self.rect.bottomright
+        l, t, r, b = self.to_screen(self.player.rect.rect)
+        if l < bl or r > br or b > bb:
             self.die()
+
+    def win (self):
+        self.ID += 1
+        if self.ID == len(conf.LEVELS):
+            self.game.quit()
+        else:
+            self.init()
 
     def die (self):
         self.init()
@@ -138,14 +155,24 @@ class Level:
 
     def draw (self, screen):
         screen.fill((0, 0, 0))
-        screen.fill((255, 255, 255), self.win)
+        screen.fill((255, 255, 255), self.window)
         rects = []
         for r in self.rects:
             col = self.to_screen(r.pgrect)
-            col = self.win.clip(col)
+            if self.show:
+                col = pg.Rect(col)
+            else:
+                col = self.window.clip(col)
             if col:
                 screen.fill((50, 50, 50), col)
+                if self.show:
+                    col = self.window.clip(col)
+                    if not col:
+                        continue
                 rects.append(col)
+        # goal
+        screen.fill((255, 255, 0), self.to_screen(self.goal.pgrect))
+        # player
         p = self.to_screen(self.player.rect.pgrect)
         screen.fill((255, 0, 0), p)
         for r in rects:
